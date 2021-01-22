8749c28bd69f3a64a3d83f5279ac30822e4c21f0
Víctor Gómez <vgomez@emfasi.com>
2021-01-22 13:47:11 +0100

    complementos

---
+++ src/components/components-complementos.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+//a este no le pasaremos nada.
+export default function componentevacio() {
+    return (
+        <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+            <p>Este es un componente que es codigo estatico, sin ninguna varible, a este complemento se le llama asi:</p>
+            <img src='/src/images/sincomplemento.PNG'></img>
+        </div>
+    )
+}
