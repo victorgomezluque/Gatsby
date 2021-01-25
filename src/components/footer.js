fa7749804aceea310cdd38b51be898c2d277e9ce
Víctor Gómez <vgomez@emfasi.com>
2021-01-25 08:05:20 +0100

    Footer

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+//a este no le pasaremos nada.
+export default function footer() {
+    return (
+        <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+        <p>footer</p>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Footer from "../components/footer"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
@@ -14,6 +15,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <Footer></Footer>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
